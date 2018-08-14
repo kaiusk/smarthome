6dd3c59ae26da2e8284623a1b81d9f3ed36c5c0e
kaiusk <kaiuskaron@gmail.com>
2018-08-14 10:40:59 +0300

    Add files via upload

---
+++ trepp_fade.ino
@@ -0,0 +1,36 @@
+#include <SparkFun_Tlc5940.h>
+#include <tlc_fades.h>
+
+#define sensorLight_PIN A5                       // Light sensor  Do not change!!!
+#define sensorBottom_PIN A1                      // Bottom sensor Do not change!!!                     
+#define sensorUpper_PIN A2                       // Upper sensor  Do not change!!!
+boolean sensorUpperActive;
+boolean sensorBottomActive;
+TLC_CHANNEL_TYPE channel;
+
+void setup() {
+  Tlc.init();
+  pinMode(sensorBottom_PIN, INPUT);
+  pinMode(sensorUpper_PIN, INPUT);
+  pinMode(sensorLight_PIN, INPUT);
+  Serial.begin(9600);
+}
+
+
+
+void loop() {
+    uint32_t startMillis = 0; //millis();
+    uint32_t endMillis = 0;//startMillis + duration;
+    
+    if (tlc_fadeBufferSize < TLC_FADE_BUFFER_LENGTH - 2) {
+      for (channel = 0; channel<16; channel++) {
+        if (!tlc_isFading(channel)) {
+            startMillis = millis() + (channel * 100); // mitu ms hiljem alustab järgmine aste
+            endMillis = startMillis + 500; // mitu ms kestab ühe aste fade
+            tlc_addFade(channel, 0, 4095, startMillis, endMillis);
+        }
+    }
+  }
+  tlc_updateFades();
+}
+

86b1f4b58dbe27c30a53149fd39205f9439e28a4
kaiusk <kaiuskaron@gmail.com>
2018-08-15 10:07:24 +0300

    Update trepp_fade.ino

--- trepp_fade.ino
+++ trepp_fade.ino
@@ -1,56 +1,78 @@
 #include <SparkFun_Tlc5940.h>
 #include <tlc_fades.h>
 
-#define sensorLight_PIN A5                       // Light sensor  Do not change!!!
-#define sensorBottom_PIN A1                      // Bottom sensor Do not change!!!                     
-#define sensorUpper_PIN A2                       // Upper sensor  Do not change!!!
+#define sensorLight_PIN 1
+#define sensorBottom_PIN 2
+#define sensorUpper_PIN 3
+
 boolean sensorUpperActive;
 boolean sensorBottomActive;
+boolean fadeActive;
 TLC_CHANNEL_TYPE channel;
 uint32_t startMillis; 
-int yhe_astme_aeg = 500;
-int koik_polevad = 10000;
-int astmete_vaheline_aeg = 200;
+int oneLEDtime = 500;
+int allLEDon = 10000;
+int delayBetweenLED = 200;
 
 void setup() {
-  Tlc.init();
-  pinMode(sensorBottom_PIN, INPUT);
-  pinMode(sensorUpper_PIN, INPUT);
-  pinMode(sensorLight_PIN, INPUT);
-  Serial.begin(9600);
+    Tlc.init();
+    pinMode(sensorBottom_PIN, INPUT);
+    digitalWrite (sensorBottom_PIN, LOW);
+    pinMode(sensorUpper_PIN, INPUT);
+    digitalWrite (sensorUpper_PIN, LOW);
+    pinMode(sensorLight_PIN, INPUT);
+    //Serial.begin(9600);
+}
+
+void BottomTriggerFire() {
+    if (digitalRead (sensorBottom_PIN) && !fadeActive) {      //Dependof the sensor type, if 0 when triggered ,than change the comparison to opposite value
+        sensorBottomActive = true;
+    }
+}
+
+void UpperTriggerFire() {
+    if (digitalRead (sensorUpper_PIN) && !fadeActive) {      //Dependof the sensor type, if 0 when triggered ,than change the comparison to opposite value
+        sensorUpperActive = true;
+    }
 }
 
 void setChannel(TLC_CHANNEL_TYPE ch) {
-    if (!tlc_isFading(ch)) {
-        startMillis = millis() + (ch * astmete_vaheline_aeg); // mitu ms hiljem alustab järgmine aste
-        // fadein
-        tlc_addFade(ch, 0, 4095, startMillis, startMillis + yhe_astme_aeg); // mitu ms kestab ühe aste fade
-        // fadeout
-        tlc_addFade(ch, 4095, 0, startMillis + koik_polevad, startMillis + koik_polevad + yhe_astme_aeg ); // mitu ms kestab ühe aste fade
-    }
+    // fadein
+    tlc_addFade(ch, 0, 4095, startMillis, startMillis + oneLEDtime); // mitu ms kestab ühe aste fade
+    // fadeout
+    tlc_addFade(ch, 4095, 0, startMillis + allLEDon, startMillis + allLEDon + oneLEDtime ); // mitu ms kestab ühe aste fade
 }
 
-void liiguYles() {
+void moveUp() {
     if (tlc_fadeBufferSize < TLC_FADE_BUFFER_LENGTH - 2 && sensorBottomActive) {
         for (channel = 0; channel < 16; channel++) {
-            setChannel(channel);
+            if (!tlc_isFading(ch)) {
+                startMillis = millis() + (ch * delayBetweenLED); 
+                setChannel(ch);
+            }
         }
+        sensorBottomActive = false;
     }
-    tlc_updateFades();
 }
 
-void liiguAlla() {
+void moveDown() {
     if (tlc_fadeBufferSize < TLC_FADE_BUFFER_LENGTH - 2 && sensorUpperActive) {
         for (channel = 15; channel < 1; channel--) {
-            setChannel(channel);
+            if (!tlc_isFading(ch)) {
+                startMillis = millis() + ((15 - ch) * delayBetweenLED);
+                setChannel(ch);
+            }
         }
+        sensorUpperActive = false;        
     }
-    tlc_updateFades();
 }
 
 void loop() {
-    liiguYles();
-    delay(10000);
-    liiguAlla();
-    delay(10000);
+    if (digitalRead (sensorLight_PIN)) {        //Dependof the sensor type, if 0 when triggered ,than change the comparison to opposite value
+        BottomTriggerFire();
+        UpperTriggerFire();
+        moveUp();
+        moveDown();
+    }
+    fadeActive = tlc_updateFades();
 }

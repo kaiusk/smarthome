75d13057a147d7fd7a70fd87e77cd2bccca3639a
kaiusk <kaiuskaron@gmail.com>
2018-08-16 14:09:05 +0300

    Update Saunatemp.ino

--- Saunatemp.ino
+++ Saunatemp.ino
@@ -5,27 +5,24 @@
 #define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-DeviceAddress saun;
 
 TLC_CHANNEL_TYPE channel;
 
-int arvud[10][7] = {
-    // a,b,b,d,e,f,g
-    {1,1,1,1,1,1,0}, // 0
-    {0,1,1,0,0,0,0}, // 1
-    {1,1,0,1,1,0,1}, // 2
-    {1,1,1,1,0,0,1}, // 3
-    {0,1,1,0,0,1,1}, // 4
-    {1,0,1,1,0,1,1}, // 5
-    {1,0,1,1,1,1,1}, // 6
-    {1,1,1,0,0,0,0}, // 7
-    {1,1,1,1,1,1,1}, // 8
-    {1,1,1,1,0,1,1}  // 9
+int arvud[10] = {
+  B1111110, // 0
+  B0110000, // 1
+  B1101101, // 2
+  B1111001, // 3
+  B0110011, // 4
+  B1011011, // 5
+  B1011111, // 6
+  B1110000, // 7
+  B1111111, // 8
+  B1111011  // 9
 };
 
 void setup() {
     sensors.begin();
-    sensors.getAddress(saun, 0);
     Tlc.init();
     //Serial.begin(9600);
 }
@@ -42,7 +39,7 @@ void dec_to_lcd(int led, int nr) {
     
     int a = arvud[nr];
     for (int i = 0; i < 8; i++) {
-        int heledus = 4095 * a[i];
+        int heledus = 4095 * bitRead(a, i);
         channel = i + 8 * led;
         Tlc.set(channel, heledus);
     }
@@ -52,9 +49,8 @@ void dec_to_lcd(int led, int nr) {
 
 void loop() {
     sensors.requestTemperatures();
-    tempC = sensors.getTempC(saun);
-  
-    tmp = int(tempC*10);
+    int tempC = sensors.getTempCByIndex(0);
+    int tmp = int(tempC*10);
     
     dec_to_lcd(1, (int(tmp)%100)/10);
     dec_to_lcd(0, (int(tmp)%100)%10);
@@ -67,4 +63,3 @@ void loop() {
         delay(2000);
     }*/
 }
-
